af04621709c0e51ea2d59ca128b9c0754b1fb837
John <jamesbondofficial@protonmail.com>
2023-10-27 20:48:27 -0400

    Trying to add winning animation

--- java.js
+++ java.js
@@ -35,7 +35,7 @@ const gameboard = (() => {
     function checkDrawWin(currentPlayer){
 
         // If 3 in a row do this;
-        if (checkRowWin(currentPlayer) ) {
+        if (checkRowWin(currentPlayer) != null ) { // ADDED != NULL
 
             // Figuring out which player got 3 in a row
             const p = currentPlayer == 'X' ? p1 : p2 
@@ -45,12 +45,33 @@ const gameboard = (() => {
 
             // Updating the scoreboard text content
             if (p == p1) {
-                scoreBoardP1.textContent = `P1: ${p.score}`
+                scoreBoardP1.textContent = `Player X: ${p.score}`
             } else {
-                scoreBoardP2.textContent = `P1: ${p.score}`
+                scoreBoardP2.textContent = `Player O: ${p.score}`
             }
 
-            boardReset();
+            /* boardReset(); */ // Temporary comment out
+
+            winningCellsIndices.forEach(index => {
+                const winningCell = document.getElementById(`cell-${index}`); // Replace with your cell identifier
+                winningCell.classList.add('winning-cell'); // Add a class for styling
+            });
+
+            const winningCells = document.querySelectorAll('.winning-cell'); // Replace with actual selector
+
+            // Apply the animation to each winning cell
+            winningCells.forEach(cell => {
+            cell.classList.add('winning-animation'); // Add the CSS class with the animation
+            });
+
+            // Remove the animation class when the animation ends
+            winningCells.forEach(cell => {
+                cell.addEventListener('animationend', () => {
+                cell.classList.remove('winning-animation');
+                });
+            });
+  
+
         
         // Checking if there's a draw after concluding no 3 in a row.
         } else if(gameBoard.includes("") == false){
@@ -70,7 +91,7 @@ const gameboard = (() => {
         })
 
         isPlayer1Turn = true; // Reset the player turn, assuming player 1 starts
-        
+
         gameBoard = Array(9).fill(""); // Reset the gameboard array
 
         // gameBoard = ["", "", "", "", "", "", "", "", ""];
@@ -79,7 +100,19 @@ const gameboard = (() => {
     // Check for a win handle game logic
     // First 3 horizontal rows, 3 vertical rows, 2 diagonal rows.
     function checkRowWin(player) {
-        return (
+
+        // Added all this
+        for (const combination of winningCombinations) {
+            const [a, b, c] = combination;
+            if (gameBoard[a] === player && gameBoard[b] === player && gameBoard[c] === player) {
+              // Store the indices of the winning cells
+              const winningCellsIndices = combination;
+              return winningCellsIndices;
+            }
+        }
+        return null; // No win
+
+        /* return (
             (gameBoard[0] === player && gameBoard[1] === player && gameBoard[2] === player) ||
             (gameBoard[3] === player && gameBoard[4] === player && gameBoard[5] === player) ||
             (gameBoard[6] === player && gameBoard[7] === player && gameBoard[8] === player) ||
@@ -88,8 +121,16 @@ const gameboard = (() => {
             (gameBoard[2] === player && gameBoard[5] === player && gameBoard[8] === player) ||
             (gameBoard[0] === player && gameBoard[4] === player && gameBoard[8] === player) ||
             (gameBoard[6] === player && gameBoard[4] === player && gameBoard[2] === player)
-        );
+        ); */
     }
+
+
+
+    const winningCombinations = 
+    [[0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows 
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
 
     // MAYBE MAKE A FUNCTION TO CHECK FOR DRAW?
 
@@ -105,6 +146,16 @@ const gameboard = (() => {
     function makeMove(cell, index){
         // Determine who's turn it is
         const currentPlayer = isPlayer1Turn ? "X" : "O"
+
+        // Change background color depending on who's turn
+
+        if (currentPlayer == "X") {
+            scoreBoardP1.style.backgroundColor = 'red'
+            scoreBoardP2.style.backgroundColor = 'green'
+        } else {
+            scoreBoardP1.style.backgroundColor = 'green'
+            scoreBoardP2.style.backgroundColor = 'red'
+        }
 
         // Update the gameboard array and cell content
         gameBoard[index] = currentPlayer;
